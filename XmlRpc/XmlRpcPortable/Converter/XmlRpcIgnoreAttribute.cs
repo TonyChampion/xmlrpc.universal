622c34a5e0360a0219e8bc7988f636c9c361c69d
TonyChampion <tonyc@championds.com>
2016-10-14 11:11:53 -0500

    Adding missing files

---
+++ XmlRpc/XmlRpcPortable/Converter/XmlRpcIgnoreAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlRpcPortable.Converter
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class XmlRpcIgnoreAttribute : Attribute
+    {
+        public XmlRpcIgnoreAttribute()
+        {
+        }
+    }
+}
